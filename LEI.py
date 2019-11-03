3cba7296c6429ef917df915f168b132d59c46634
mytest <1347731131@qq.com>
2019-11-03 14:15:00 +0800

    Initial commit

---
+++ LEI.py
@@ -0,0 +1,9 @@
+for i in range(6):
+    # 总体思路是，先打印一行空行，代表每一行星星前的空格
+    # 在不换行，打印星号
+    for j in range(6-i):
+        print(" ",end="")
+    for m in range(i+1):
+        print("* ", end="")
+        # 打印完一行后换行
+    print()
